67c06565d69c89294f0254e7708b2ed97f9a25a8
Josh <joshualeite2003@gmail.com>
2024-06-17 12:25:57 +0800

    Set Up visuals for scene
    Now that I've brought all the visuals into the scene its time to start working on the scripts

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public int MoneyValue = 0;
+    public int bet = 0;
+
+    [SerializeField]
+    //put in text field here for outcome of spin
+
+    void Update()
+    {
+        
+    }
+}
